80e50fc27de6fb6b6171664056ceea6ef4fc60ac
lemniscate77 <lemniscate77@users.noreply.github.com>
2020-08-01 00:09:17 +0300

    три файла

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,28 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private String userName = "root";
+    private String password = "12345iB";
+    private String connectionUrl = "jdbc:mysql://localhost:3306/lesson113?serverTimezone=Europe/Moscow&useSSL=false";
+    Connection connection = null;
+
+    public Connection setConnection() {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            connection = DriverManager.getConnection(connectionUrl, userName, password);
+            System.out.println("We're connected");
+        } catch (SQLException e) {
+            System.out.println("Didn't connected (class Util)");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
