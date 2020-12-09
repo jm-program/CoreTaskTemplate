2d47507d660b0c8b96e96fdb3768a06db6f8452c
VitaliyKrivoshchapov <69453241+VitaliyKrivoshchapov@users.noreply.github.com>
2020-12-09 21:24:10 +0500

    подключение в классе Util  и вывод из базы

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,52 +1,57 @@
 package jm.task.core.jdbc;
 
 import com.mysql.fabric.jdbc.FabricMySQLDriver;
+import jm.task.core.jdbc.util.Util;
 
 
-
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.net.PasswordAuthentication;
+import java.sql.*;
 
 
 public class Main {
     //static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String URL ="jdbc:mysql://localhost:3306/mydbtest?autoReconnect=true&useSSL=false";
+    private static final String URL ="jdbc:mysql://localhost:3306/business?autoReconnect=true&useSSL=false";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
     public static void main(String[] args) throws SQLException {
-        Connection connection = null;
-        Driver driver;
 
-        try {
-            driver = new FabricMySQLDriver();
-            System.out.println("Удалось загрузить драйвер");
-        } catch (SQLException throwables) {
-            System.out.println("Не удалось загрузиь драйвер");
-            return;// дальше нечего выполнять
-        }
-        try {
-            DriverManager.registerDriver(driver);
-            System.out.println("Удалось зареистрировать драйвер");
-        } catch (SQLException throwables) {
-            System.out.println("Не удалось зарегистрировать драйвер");
-            return;
-        }
-        try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            System.out.println("Создали коннект и рабоаем");
-        } catch(SQLException ex){
-            System.out.println("НЕ удалось создать соединение");
-            return;
-        }
-        finally {
-            if(connection!=null)
-                connection.close();
+        Util util = new Util();
+        Connection conn = util.getConnection(URL,USERNAME,PASSWORD);
+        Statement statement = conn.createStatement();
+
+        ResultSet resSet = statement.executeQuery("select * from business.products");
+
+        while (resSet.next()){
+            int id;
+            String name;
+            double price;
+            int shopID;
+            id = resSet.getInt("product_id");
+            name = resSet.getString("product_name");
+            price = resSet.getDouble("price");
+            shopID = resSet.getInt("shop_id");
+            Product product = new Product(id, name, price, shopID);
+
+            System.out.println(product);
         }
 
-        //final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-        // реализуйте алгоритм здесь
+        statement.close();
+        conn.close();
+
+       /* public void createUsersTable() {
+            String SQL = "CREATE TABLE IF NOT EXISTS `jmDatabase`.`" + table + "` (\n" +
+                    "  `id` ong NOT NULL AUTO_INCREMENT,\n" +
+                    "  `name` VARCHAR(45) NULL,\n" +
+                    "  `lastname` VARCHAR(45) NULL,\n" +
+                    "  `age` TINYINT NULL,\n" +
+                    "  PRIMARY KEY (`id`));";
+            Statement statement = new Util;
+            try {
+                statement.executeUpdate(SQL);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }*/
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,33 @@
 package jm.task.core.jdbc.util;
 
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL ="jdbc:mysql://localhost:3306/business?autoReconnect=true&useSSL=false";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    private Connection connection;
+      public Util(){// в конструкторе регистрируем драйвер, а соединение в методе
+          try {
+              DriverManager.registerDriver(new FabricMySQLDriver());
+          } catch (SQLException ex) {
+              System.out.println("Ошибка регистриции драйвера");
+              return;
+          }
+      }
+
+    public Connection getConnection(String url, String username, String password) throws SQLException {
+        if (connection != null)
+            return connection;
+        connection = DriverManager.getConnection(url, username, password);
+        System.out.println("Соединение установленно");
+        return connection;
+      }
+
 }
