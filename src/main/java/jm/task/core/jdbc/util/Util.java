30393c899d47519aafa9a6ed2e2653eb6b5f3a2a
Artem Kulin <kulinartem@gmail.com>
2021-01-02 19:39:58 +0300

    added realization of Util.class

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,15 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    public static void driverRegister () {
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 }
