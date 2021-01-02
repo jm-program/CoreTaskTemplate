49ed93c62d28f2e2af063cf8d3150e2dd59e2441
Artem Kulin <kulinartem@gmail.com>
2021-01-02 19:41:38 +0300

    added realization of UserServiceImpl.class

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,34 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
+    private static final UserDaoJDBCImpl userDAO = new UserDaoJDBCImpl();
 
+    public void createUsersTable() {
+        userDAO.createUsersTable();
     }
 
     public void dropUsersTable() {
-
+        userDAO.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        userDAO.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-
+        userDAO.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return null;
+        return userDAO.getAllUsers();
     }
 
     public void cleanUsersTable() {
-
+        userDAO.cleanUsersTable();
     }
 }
