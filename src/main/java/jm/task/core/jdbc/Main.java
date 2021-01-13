0ab033b7a4d9a43209644b32d5491ba55df9194b
Igor Belogolovsky <igor@belogolovsky.net>
2021-01-13 21:09:55 +0300

    Tested connection to db

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,21 @@
 package jm.task.core.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class Main {
+    private static final String URL = "jdbc:mysql://localhost/dbtest?serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
