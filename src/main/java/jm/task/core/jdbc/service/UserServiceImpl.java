18715c97064287f06a15100aedfc0875540157f4
Ohr42 <ohr79@mail.ru>
2021-02-23 20:50:30 +0300

    1.1.3_v.3 Moved implementation of methods to Dao

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,16 +1,9 @@
 package jm.task.core.jdbc.service;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
-import jm.task.core.jdbc.util.Util;
 
 public class UserServiceImpl extends UserDaoJDBCImpl implements UserService {
   public void createUsersTable() {
