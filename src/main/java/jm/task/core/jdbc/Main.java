1f05837b85da2a901cdde03000561d8a1c4c0bba
Ohr42 <ohr79@mail.ru>
2021-02-24 20:24:42 +0300

    PPT_1.1.4 v.1.1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
@@ -16,11 +17,12 @@ public class Main {
         us.saveUser("Petr","Petrov",(byte)14);
         us.saveUser("Gleb","Zemnuhov",(byte)17);
         us.saveUser("Ivan","Petrov",(byte)34);
-//      System.out.println();
-//      for (User u: us.getAllUsers()){
-//        System.out.println(u.toString());
-//      }
-//      us.cleanUsersTable();
+        us.removeUserById(3L);
+
+        for (User u: us.getAllUsers()){
+          System.out.println(u);
+        }
+      us.cleanUsersTable();
       us.dropUsersTable();
 
       }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -35,28 +35,83 @@ public class UserDaoHibernateImpl extends UserDaoJDBCImpl implements UserDao {
             session.beginTransaction();
             session.save(user);
             session.getTransaction().commit();
+            String addUser = new String("User с именем – " + user.getName() +" добавлен в базу данных");
+            System.out.println(addUser);
         }catch (Exception e){
             System.out.println("Ошибка saveUser: " + e);
         }finally {
             try{session.close();}catch (Exception ignored){};
-            try{sessionFactory.getCurrentSession().close();}catch (Exception ignored){};
-        }
+            try{
+                sessionFactory.getCurrentSession().close();
 
+            }catch (Exception ignored){};
+        }
 
     }
 
     @Override
     public void removeUserById(long id) {
+        SessionFactory sessionFactory = null;
+        Session session = null;
+        try {
+            sessionFactory = Util.getSessionFactory();
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            User user = (User) session.load(User.class, id);
+            session.delete(user);
+            session.getTransaction().commit();
 
+        }catch (Exception e){
+            System.out.println("Ошибка в removeUserById: "+e);
+        }finally {
+            try{session.close();}catch (Exception ignored){};
+            try{
+                sessionFactory.getCurrentSession().close();
+            }catch (Exception ignored){};
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        SessionFactory sessionFactory = null;
+        Session session = null;
+        List<User> list = null;
+        try {
+            sessionFactory = Util.getSessionFactory();
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            list = session.createQuery("from User").list();
+            session.getTransaction().commit();
+
+        }catch (Exception e){
+            System.out.println("Ошибка в removeUserById: "+e);
+        }finally {
+            try{session.close();}catch (Exception ignored){};
+            try{
+                sessionFactory.getCurrentSession().close();
+            }catch (Exception ignored){};
+        }
+        return list;
     }
 
     @Override
     public void cleanUsersTable() {
+        SessionFactory sessionFactory = null;
+        Session session = null;
+        try {
+            sessionFactory = Util.getSessionFactory();
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            session.createQuery("delete from User").executeUpdate();
+            session.getTransaction().commit();
 
+        }catch (Exception e){
+            System.out.println("Ошибка в removeUserById: "+e);
+        }finally {
+            try{session.close();}catch (Exception ignored){};
+            try{
+                sessionFactory.getCurrentSession().close();
+            }catch (Exception ignored){};
+        }
     }
 }
