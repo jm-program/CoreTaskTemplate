535e98f8bc0692d7a9bfcbb872896e2a2783220a
molluk <molluk@mail.ru>
2021-03-03 11:35:57 +0300

    03.03.2021 11:35

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,10 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        Util.getUtilConnection();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    static private String URL = "jdbc:mysql://localhost:3306/database";
+    static private String URL_FIX = "jdbc:mysql://localhost:3306/database?useUnicode=true&useSSL=false&" +
+            "useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    static private String LOG = "root";
+    static private String PASS = "root";
+    private static Connection connection;
+
+    public Util(){   }
+
+    public static Connection getUtilConnection() { // геттер приватного поля connection + создаем connection
+        try {
+            connection = DriverManager.getConnection(URL_FIX, LOG, PASS);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return connection;
+    }
+
 }
