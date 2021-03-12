6492cebe7549a4b40e2a5066707f9e6238305457
Alexey <slag88@mail.ru>
2021-03-12 12:25:03 +0300

    connection_method

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,35 +8,30 @@ import java.sql.Statement;
 public class Util {
     // реализуйте настройку соеденения с БД
 
-    public static void main(String[] args) {
-        try{
             String url = "jdbc:mysql://localhost:3306/ultra_low?useSSL=false";
             String username = "rootalex";
             String password = "rootalex";
 
            // Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = null;
-            try (Connection connection = DriverManager.getConnection(url, username, password)){
-
+             Connection getConnection() {
+                Connection connection = null;
+            try {
+                connection = DriverManager.getConnection(url, username, password);
                 System.out.println("Connection to Store DB succesfull!");
-                //1
-                //2
-                //3
-                //new 10-03//
                 try {
-                    Statement statement =connection.createStatement();
+                    Statement statement = connection.createStatement();
                     //вставка в таблицу
                     statement.execute("INSERT INTO developers(name, salary) VALUES('biba', 100500);");
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
                 //new end//
-            }
-        }
-        catch(Exception ex){
-            System.out.println("Connection failed...");
+            } catch (Exception ex) {
+                System.out.println("Connection failed...");
 
-            System.out.println(ex);
-        }
-    }
+                System.out.println(ex);
+            }
+            return connection;
+         }
 }
+
