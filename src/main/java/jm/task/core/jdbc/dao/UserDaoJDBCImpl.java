ab02df98117df1ee927bdb3cd6d7e0a3b310ee28
Alexey <slag88@mail.ru>
2021-03-12 15:33:15 +0300

    method_2_statement_prepare_primary_rdy

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -25,12 +25,15 @@ public class UserDaoJDBCImpl implements UserDao {
         }
         try {
             //statement.execute("INSERT INTO developers(name, salary) VALUES('biba', 100500);");
-            statement.execute("CREATE TABLE User(\n" +
-                    "   id INT,\n" +
+            statement.execute("CREATE TABLE User3(\n" +
+                    //auto_increment + primare key, для генерации id
+                    "   id INT AUTO_INCREMENT,\n" +
+                    "   PRIMARY KEY (id), \n" +
                     "   name VARCHAR(50),\n" +
                     "   lastName VARCHAR(50),\n" +
                     "   age INT\n" +
                     ")");
+
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
@@ -43,20 +46,21 @@ public class UserDaoJDBCImpl implements UserDao {
     }
     //Добавление 4 User(ов) в таблицу с данными на свой выбор
     public void saveUser(String name, String lastName, byte age) {
-        int id = 1;
+       // int id = 1;
         Util util = new Util();
         Connection connection = util.getConnection();
         //PreparedStatement preparedStatement = null;
         try {
             //String sql = "INSERT INTO user (name, lastName, age) VALUES (?, ?, ?)";
             PreparedStatement preparedStatement = connection.prepareStatement
-                    ("INSERT INTO  user (id, name, lastName, age) VALUES(?, ?, ?, ?);");
+                    ("INSERT INTO  user3 (name, lastName, age) VALUES(?, ?, ?);");
             //preparedStatement = connection.prepareStatement();
-            preparedStatement.setInt(1, id);
-            preparedStatement.setString(2, name);
-            preparedStatement.setString(3, lastName);
-            preparedStatement.setInt(4, age);
+            //preparedStatement.setInt(1, id);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setInt(3, age);
 
+            //закидывает данные в table
             preparedStatement.executeUpdate();
 
         } catch (SQLException throwables) {
