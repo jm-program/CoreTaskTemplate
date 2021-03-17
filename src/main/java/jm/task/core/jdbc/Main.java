88d6d6d7998f2291357b3bd0ba3615aca612ef88
mr-nicko89 <mr-nicko89@yandex.ru>
2021-03-17 17:17:55 +0300

    Функционал работы с БД перенесен в dao. Реализован необходимый по заданию функционал в main. Установил SQL Dialect = MySQL.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -22,9 +22,15 @@ public class Main {
         try {
             userService.createUsersTable();
 
-            userService.saveUser("Stive2", "Libovski", (byte) 43);
-            userService.saveUser("Stive3", "Libovski", (byte) 43);
-            userService.saveUser("Stive4", "Libovski", (byte) 43);
+            userService.saveUser("Stive1", "Libovski", (byte) 43);
+            userService.saveUser("Stive2", "Libovski", (byte) 44);
+            userService.saveUser("Stive3", "Libovski", (byte) 45);
+            userService.saveUser("Stive4", "Libovski", (byte) 46);
+
+            System.out.println("Вывод все пользователей в консоль \n "+ userService.getAllUsers().toString());
+
+            userService.cleanUsersTable();
+            userService.dropUsersTable();
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
