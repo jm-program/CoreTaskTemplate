ae6877f644e9de277d7c7604d35fa109bc657158
NetPatrol <madraddog@gmail.com>
2021-06-15 04:34:17 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -5,7 +5,6 @@ import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.sql.SQLException;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
