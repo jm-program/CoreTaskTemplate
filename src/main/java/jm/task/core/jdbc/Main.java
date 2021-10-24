22fe588baef814b427aa08797f08a7f9b023e621
GSteffun <42888439+GSteffun@users.noreply.github.com>
2021-10-24 14:48:05 +0300

    Test

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,17 +8,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-//        UserService userService = new UserServiceImpl();
-//        userService.createUsersTable();
-//        userService.saveUser("Vladimir", "Nuroev", (byte)53);
-//        userService.saveUser("Vadim", "Rolnin", (byte)37);
-//        userService.saveUser("Stanislav", "Petrov", (byte)44);
-//        userService.saveUser("Oleg", "Igorev", (byte)29);
-//        List<User> listUsers = userService.getAllUsers();
-//        System.out.println(listUsers);
-//        userService.cleanUsersTable();
-//        userService.dropUsersTable();
-
+        //This is a branch 114
         UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
         userDaoHibernate.createUsersTable();
         userDaoHibernate.saveUser("Vasily", "Petrov", (byte)48);
@@ -30,5 +20,4 @@ public class Main {
         userDaoHibernate.cleanUsersTable();
         userDaoHibernate.dropUsersTable();
     }
-
 }
