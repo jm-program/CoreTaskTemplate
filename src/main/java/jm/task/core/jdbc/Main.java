a8c2cd8c6f6a5bdc6b7bc268bda43429b5200886
KaroshiInside <rpatrushevv@yandex.ru>
2022-01-06 22:42:08 +0700

    q1done?

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,18 +7,18 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl daoHibernate = new UserServiceImpl();
+        UserDao daoHibernate = new UserDaoHibernateImpl();
         daoHibernate.createUsersTable();
         daoHibernate.saveUser("Roman", "Patrushev", (byte) 26);
         daoHibernate.saveUser("Vladimir", "Putin", (byte) 69);;
         daoHibernate.saveUser("Andrey", "Gubin", (byte) 47);
         daoHibernate.saveUser("Pjer", "Narciss", (byte) 44);
-        List<User> users = daoHibernate.getAllUsers();
-        System.out.println(users);
+        System.out.println(daoHibernate.getAllUsers());
         daoHibernate.cleanUsersTable();
         daoHibernate.dropUsersTable();
     }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,35 +1,36 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDaoHibernateImpl dao = new UserDaoHibernateImpl();
+    private UserDao userDao = new UserDaoHibernateImpl();
 
     public void createUsersTable() {
 
-        dao.createUsersTable();
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        dao.dropUsersTable();
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        dao.saveUser(name, lastName, age);
+        userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        dao.removeUserById(id);
+        userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return dao.getAllUsers();
+        return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        dao.cleanUsersTable();
+        userDao.cleanUsersTable();
     }
 }
